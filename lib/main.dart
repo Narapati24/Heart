82ca26b0460c61c082ac5881fdc5a95f9704edce
aldiipratama <paldi0013@gmail.com>
2024-01-12 21:20:09 +0700

    add slicing screen verification code

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,8 @@
 // ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
 
 import 'package:flutter/material.dart';
-import 'package:heart/screens/login.dart';
+import 'package:heart/screens/phone_number.dart';
+// import 'package:heart/screens/login.dart';
 
 void main() {
   runApp(const HeartApp());
@@ -14,7 +15,7 @@ class HeartApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Login(),
+      home: PhoneNumber(),
     );
   }
 }
