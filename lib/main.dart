1561d14470a0ccf3f40ad19b7e649dbe5ee25a40
aldiipratama <paldi0013@gmail.com>
2024-01-18 12:24:43 +0700

    solve

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,7 @@
 // ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
 
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_onboarding_slider/flutter_onboarding_slider.dart';
-import 'package:heart/screens/onboarding.dart';
+import 'package:heart/screens/login.dart';
 
 void main() {
   runApp(const HeartApp());
@@ -14,34 +12,8 @@ class HeartApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return CupertinoApp(
-      home: OnBoardingSlider(
-        totalPage: 3,
-        headerBackgroundColor: Colors.white,
-        background: [
-          Image.asset('assets/images/illustration-slide1.png'),
-          Image.asset('assets/images/illustration-slide2.png'),
-          Image.asset('assets/images/illustration-slide3.png'),
-        ],
-        speed: 2,
-        pageBodies: [
-          Container(
-            alignment: Alignment.bottomCenter,
-            height: 200,
-            child: const Text('data'),
-          ),
-          Container(
-            alignment: Alignment.bottomCenter,
-            height: 200,
-            child: const Text('data'),
-          ),
-          Container(
-            alignment: Alignment.bottomCenter,
-            height: 200,
-            child: const Text('data'),
-          ),
-        ],
-      ),
+    return MaterialApp(
+      home: Login(),
     );
   }
 }
